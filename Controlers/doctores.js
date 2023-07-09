8ff877b0e32ec4b216410bcbfe8f90249867bd40
santiago rojas <santiago_bogota@hotmail.com>
2023-07-09 15:04:40 -0500

    Creamos una carpeta Controlers,donde manejaremos las rutas para pacientes y doctores, en doctores  tenemos rutas para ver todos los doctores y su informacion y por su ID ,agregar doctores y eliminar

---
+++ Controlers/doctores.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const connection = require('../db');
+const doctores = express.Router();
+
+// Ruta para crear un doctor
+doctores.post("/", async (req, res) => {
+    try {
+      const {
+        idDoctores,
+        Name_Doc,
+        Apellido_Doc,
+        Correo_Elec,
+        Consultorio,
+        id_Especialidades,
+      } = req.body;
+  
+      const sql =
+        "INSERT INTO Doctores (idDoctores,Name_Doc,Apellido_Doc,Correo_Elec,Consultorio,id_Especialidades) VALUES (?, ?, ?, ?, ?, ? )";
+      const values = [
+        idDoctores,
+        Name_Doc,
+        Apellido_Doc,
+        Correo_Elec,
+        Consultorio,
+        id_Especialidades,
+      ];
+  
+      await new Promise((resolve, reject) => {
+        connection.query(sql, values, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+  
+      res.redirect(301, "/Doctores");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Error al crear el doctor");
+    }
+  });
+
+  //Ruta para ver listado de Doctores
+doctores.get("/", (req, res) => {
+    const sql = "SELECT * FROM Doctores ";
+  
+    connection.query(sql, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error al obtener los Doctores ");
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  });
+  
+  //Ruta para ver un doctor por su ID
+  doctores.get("/:id", (req, res) => {
+    const doctorId = req.params.id;
+    const sql = "SELECT * FROM Doctores WHERE idDoctores = ?";
+  
+    connection.query(sql, [doctorId], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error al obtener el doctor");
+      } else {
+        if (result.length > 0) {
+          res.status(200).json(result);
+        } else {
+          res.status(404).send("Doctor no encontrado");
+        }
+      }
+    });
+  });
+
+
+module.exports=doctores;
